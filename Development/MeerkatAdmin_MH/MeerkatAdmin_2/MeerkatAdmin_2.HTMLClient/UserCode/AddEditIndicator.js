82fc466651a07090ea995968e7885ec4a6acf7e3
Robert MacLean <robert@sadev.co.za>
2013-08-02 13:02:04 +0200

    Added the UI swop for sub category of indicator

--- Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/UserCode/AddEditIndicator.js
+++ Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/UserCode/AddEditIndicator.js
@@ -3,7 +3,7 @@
 myapp.AddEditIndicator.created = function (screen) {
     // Write code here.
     var Indicator = screen.Indicator;
-   
+
     // Can only safely do this if the Indicator is not modified.
     if (Indicator.details.entityState !== msls.EntityState.unchanged) {
         return msls.showMessageBox(
@@ -76,5 +76,62 @@ myapp.AddEditIndicator.Indicator_Delete_execute = function (screen) {
         msls.showMessageBox(error, {
             title: "Delete failed"
         });
+    });
+};
+myapp.AddEditIndicator.IndicatorType_postRender = function (element, contentItem) {
+    // Write code here.
+
+    contentItem.dataBind("value", function (newValue) {
+
+        var indicatorGroup = contentItem.screen.findContentItem("SubIndicators");
+        $.each(indicatorGroup.children, function (index, child) {
+            child.isVisible = false;
+        });
+
+        if (newValue === undefined) {
+            return;
+        }
+
+        var target = undefined;
+        switch (newValue.Code) {
+            case "Outcome":
+                {
+                    target = contentItem.screen.findContentItem("Outcome");
+                    break;
+                }
+            case "Output":
+                {
+                    target = contentItem.screen.findContentItem("Output");
+                    break;
+                }
+            case "SubOutput":
+                {
+                    target = contentItem.screen.findContentItem("SubOutput");
+                    break;
+                }
+            case "Activity":
+                {
+                    target = contentItem.screen.findContentItem("Activity");
+                    break;
+                }
+            case "Milestone":
+                {
+                    break;
+                }
+            case "OS":
+                {
+                    break;
+                }
+            default:
+                {
+                    console.log(newValue.Code);
+                    break;
+                }
+
+        }
+
+        if (target !== undefined) {
+            target.isVisible = true;
+        }
     });
 };
