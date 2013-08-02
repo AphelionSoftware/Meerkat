863b1dea8f95444fe5a9a45ea81eca5c9ad3cf69
Robert MacLean <robert@sadev.co.za>
2013-08-02 15:13:18 +0200

    Fixed the filtering & added a decent home icon

--- Development/MeerkatCapture_MH/Meerkat_Capture/Meerkat_Capture.HTMLClient/UserCode/BrowseOutcomes.js
+++ Development/MeerkatCapture_MH/Meerkat_Capture/Meerkat_Capture.HTMLClient/UserCode/BrowseOutcomes.js
@@ -1,6 +1,13 @@
 ﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
 
-myapp.BrowseOutcomes.ShowOutcomeIndicators_Tap_execute = function (screen) {
+myapp.BrowseOutcomes.showOutcomeIndicators_execute = function (screen) {
     // Write code here.
-    this.myapp.showBrowseSubOutputs();
+    screen.getOutcomesSorted().then(function (_) {
+        if (_.selectedItem === null) {
+            alert("You must select an outcome first.");
+            return;
+        }
+
+        myapp.showBrowseIndicators(_.selectedItem.OutcomeID);
+    });
 };
