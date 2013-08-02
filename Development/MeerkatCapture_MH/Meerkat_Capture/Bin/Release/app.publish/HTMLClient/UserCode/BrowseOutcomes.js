77ed45ff432e61ee3252e79177ef23fc4b300a5f
Robert MacLean <robert@sadev.co.za>
2013-08-02 10:45:09 +0200

    Massive cleanup of index & added in missing files
    The index was in a bad way, so hopefully fixed it up correctly now that
    the .gitignore is correct.
    Added in all the 1.0.1 files for LS which were missing from the last
    commit.

--- Development/MeerkatCapture_MH/Meerkat_Capture/Bin/Release/app.publish/HTMLClient/UserCode/BrowseOutcomes.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
-
-myapp.BrowseOutcomes.ShowOutcomeIndicators_Tap_execute = function (screen) {
-    // Write code here.
-    this.myapp.showBrowseSubOutputs();
-};
