1e4ea5e03be16328ae63c34afea8932788de3998
Robert MacLean <robert@sadev.co.za>
2013-08-08 14:33:21 +0200

    Cleanup

--- Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/UserCode/AddEditIndicatorType.js
+++ Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/UserCode/AddEditIndicatorType.js
@@ -8,6 +8,7 @@ myapp.AddEditIndicatorType.IndicatorType_delete_execute = function (screen) {
 myapp.AddEditIndicatorType.IndicatorType_delete_canExecute = function (screen) {
     return msls.application.lightswitchTools.canDelete(screen);
 };
+
 myapp.AddEditIndicatorType.created = function (screen) {
     msls.application.lightswitchTools.configureCaptureForm(screen);
 };
