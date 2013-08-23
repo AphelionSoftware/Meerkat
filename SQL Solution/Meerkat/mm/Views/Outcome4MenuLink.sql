58e0352a31cf38e4e5b2ecba7b426689c524a985
Andreas Bergstedt <andreasbe@gmail.com>
2013-08-23 16:21:59 +0200

    MM View issues on Link Level

--- SQL Solution/Meerkat/mm/Views/Outcome4MenuLink.sql
+++ SQL Solution/Meerkat/mm/Views/Outcome4MenuLink.sql
@@ -1,8 +1,16 @@
-﻿CREATE VIEW [mm].[Outcome4MenuLink] 
-AS 
-  SELECT Substring(Title, 1, 50) AS Title, 
-         Link, 
-         ID, 
-         Parent 
-  FROM   mm.ALL_OutcomeMenuLink 
-  WHERE  OutcomeID = 4 
+﻿
+
+
+
+
+
+
+CREATE VIEW [mm].[Outcome4MenuLink]
+AS
+SELECT     Substring(Title,1,50) as Title, Link, ID, Parent 
+FROM mm.ALL_OutcomeMenuLink
+WHERE OutcomeID in (Select ID from (
+Select Row_Number() Over (Partition by 1 Order By Code) as IDX, OutcomeID as ID
+from app.Outcome Where Active=1 ) as IDX
+
+Where IDX.IDX=4) 
