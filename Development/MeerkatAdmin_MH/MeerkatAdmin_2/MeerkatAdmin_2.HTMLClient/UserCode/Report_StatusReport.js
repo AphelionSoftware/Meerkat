7a082782e0c8670d0aa3b3608f264b0596020786
Robert MacLean <robert@sadev.co.za>
2013-09-04 11:14:33 +0200

    Cleanup of the reporting work

--- Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/UserCode/Report_StatusReport.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
-/// <reference path="LightSwitchTools.js" />
-
-myapp.Report_StatusReport.ScreenContent_render = function (element, contentItem) {
-    msls.application.lightswitchTools.loadScript(element, "Scripts/Reports/statusreport.js");
-};
