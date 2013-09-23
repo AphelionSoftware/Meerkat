bf5361e8e5312f54f49e319b26b6fd38f8b40679
MarkGStacey <Mark.Stacey@Aphelion.WS>
2013-09-23 13:30:12 +0300

    Fixed ActiveType and DataVersion bugs
    Bugs in LightswitchTools.js
    lightswitchTools.setCommonAddScreenValues function.

--- Development/MeerkatCapture_MH/Meerkat_Capture/Meerkat_Capture.HTMLClient/UserCode/LightSwitchTools.js
+++ Development/MeerkatCapture_MH/Meerkat_Capture/Meerkat_Capture.HTMLClient/UserCode/LightSwitchTools.js
@@ -24,14 +24,19 @@
         });
 
         thisObject.ActualDate = new Date();
-
-        myapp.activeDataWorkspace.MeerkatData.DataVersions_SingleOrDefault(0).execute().then(function (results) {
+        // MGS 2013/09/32
+        /// Setting data version to 0 by default doesn't work! For demo purposes needs to be 1 as that is what the reports show
+        myapp.activeDataWorkspace.MeerkatData.DataVersions_SingleOrDefault(1).execute().then(function (results) {
+        //myapp.activeDataWorkspace.MeerkatData.DataVersions_SingleOrDefault(0).execute().then(function (results) {
             var dataVersion = results.results[0];
             thisObject.setDataVersion(dataVersion);
         });
 
         myapp.activeDataWorkspace.MeerkatData.ActiveTypes.filter("Code eq 'Active'").execute().then(function (x) {
-            thisObject.setActiveType(x.results[x]);
+            //thisObject.setActiveType(x.results[x]);
+            // MGS 2013/09/32
+            //x.results[x] will always be undefined. Instead we want the single result that should return, position 0
+            thisObject.setActiveType(x.results[0]);
         }, function (x) {
             msls.showMessageBox(x, {
                 title: "Default value for ActiveType failed"
