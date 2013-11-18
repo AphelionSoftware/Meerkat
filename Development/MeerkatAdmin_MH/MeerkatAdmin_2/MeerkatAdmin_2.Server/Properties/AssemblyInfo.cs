583cb52144e25153634c7a188d8e0913f7e2f5dc
Robert MacLean <robert@sadev.co.za>
2013-11-18 14:32:17 +0200

    Updated the reports & LS clients to the new statusvalue DB  change

--- Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.Server/Properties/AssemblyInfo.cs
+++ Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.Server/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("75cbc317-5f77-45b1-a02f-11a91f602aa4")]
 
-[assembly: AssemblyVersion("0.30.77.0")]
+[assembly: AssemblyVersion("0.30.78.0")]
 [assembly: AssemblyFileVersion("0.3.0.0")]
