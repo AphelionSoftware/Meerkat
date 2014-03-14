c6d13022589fa0b3a4c78b230d0d6a8603987977
christinaleo <christina.leo@aphelion.bi>
2014-03-14 12:06:19 +0200

    Meerkat DB - [app].[Project] cleanup

--- SQL Solution/Meerkat/app/Tables/Project.sql
+++ SQL Solution/Meerkat/app/Tables/Project.sql
@@ -6,7 +6,7 @@
     [TextDescription] VARCHAR (MAX)  NOT NULL,
     [ProjectParentID] INT            NULL,
     [OutcomeID]       INT            NOT NULL,
-    [Active]          INT            CONSTRAINT [DF__Project__Active__65570293] DEFAULT ((1)) NOT NULL,
+    [Active]          INT            CONSTRAINT [DF_Project_Active] DEFAULT ((1)) NOT NULL,
     [sys_CreatedBy]   VARCHAR (255)  CONSTRAINT [DF_Project_sys_CreatedBy] DEFAULT (user_name()) NOT NULL,
     [sys_CreatedOn]   DATETIME       CONSTRAINT [DF_Project_sys_CreatedOn] DEFAULT (getdate()) NOT NULL,
     [sys_ModifiedBy]  VARCHAR (255)  CONSTRAINT [DF_Project_sys_ModifiedBy] DEFAULT (user_name()) NOT NULL,
@@ -15,7 +15,7 @@
     CONSTRAINT [FK_Project_ActiveType] FOREIGN KEY ([Active]) REFERENCES [Core].[ActiveType] ([ID]),
     CONSTRAINT [FK_Project_Outcome] FOREIGN KEY ([OutcomeID]) REFERENCES [app].[Outcome] ([OutcomeID]),
     CONSTRAINT [FK_Project_Project] FOREIGN KEY ([ProjectParentID]) REFERENCES [app].[Project] ([ProjectID]),
-    UNIQUE NONCLUSTERED ([Code] ASC)
+    CONSTRAINT [UW_Project_Code] UNIQUE NONCLUSTERED ([Code] ASC)
 );
 
 
