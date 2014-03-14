901f4e57ebcb9fa93e0430752f082865960a7b27
christinaleo <christina.leo@aphelion.bi>
2014-03-14 11:48:52 +0200

    Meerkat DB - app.Milestone Type Cleanup

--- SQL Solution/Meerkat/app/Tables/MilestoneType.sql
+++ SQL Solution/Meerkat/app/Tables/MilestoneType.sql
@@ -2,14 +2,14 @@
     [MilestoneTypeID] INT           IDENTITY (1, 1) NOT NULL,
     [Code]            VARCHAR (50)  NOT NULL,
     [Name]            VARCHAR (255) NOT NULL,
-    [Active]          INT           CONSTRAINT [DF__Milestone__sys_A__22401542] DEFAULT ((1)) NOT NULL,
+    [Active]          INT           CONSTRAINT [DF_Milestone_Active] DEFAULT ((1)) NOT NULL,
     [sys_CreatedBy]   VARCHAR (255) CONSTRAINT [DF_MilestoneType_sys_CreatedBy] DEFAULT (user_name()) NOT NULL,
     [sys_CreatedOn]   DATETIME      CONSTRAINT [DF_MilestoneType_sys_CreatedOn] DEFAULT (getdate()) NOT NULL,
     [sys_ModifiedBy]  VARCHAR (255) CONSTRAINT [DF_MilestoneType_sys_ModifiedBy] DEFAULT (user_name()) NOT NULL,
     [sys_ModifiedOn]  DATETIME      CONSTRAINT [DF_MilestoneType_sys_ModifiedOn] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_MilestoneType] PRIMARY KEY CLUSTERED ([MilestoneTypeID] ASC),
     CONSTRAINT [FK_MilestoneType_ActiveType] FOREIGN KEY ([Active]) REFERENCES [Core].[ActiveType] ([ID]),
-    UNIQUE NONCLUSTERED ([Code] ASC)
+    CONSTRAINT [UQ_MilestoneType_Code] UNIQUE NONCLUSTERED ([Code] ASC)
 );
 
 
