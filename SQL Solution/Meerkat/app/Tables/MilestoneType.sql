84ef77077de6e39c86f1379aa361d81274d1dbbd
christinaleo <christina.leo@aphelion.bi>
2014-03-14 12:04:29 +0200

    Meerkat DB - app.MilestoneType cleanup

--- SQL Solution/Meerkat/app/Tables/MilestoneType.sql
+++ SQL Solution/Meerkat/app/Tables/MilestoneType.sql
@@ -2,7 +2,7 @@
     [MilestoneTypeID] INT           IDENTITY (1, 1) NOT NULL,
     [Code]            VARCHAR (50)  NOT NULL,
     [Name]            VARCHAR (255) NOT NULL,
-    [Active]          INT           CONSTRAINT [DF_Milestone_Active] DEFAULT ((1)) NOT NULL,
+    [Active]          INT           CONSTRAINT [DF_MilestoneType_Active] DEFAULT ((1)) NOT NULL,
     [sys_CreatedBy]   VARCHAR (255) CONSTRAINT [DF_MilestoneType_sys_CreatedBy] DEFAULT (user_name()) NOT NULL,
     [sys_CreatedOn]   DATETIME      CONSTRAINT [DF_MilestoneType_sys_CreatedOn] DEFAULT (getdate()) NOT NULL,
     [sys_ModifiedBy]  VARCHAR (255) CONSTRAINT [DF_MilestoneType_sys_ModifiedBy] DEFAULT (user_name()) NOT NULL,
