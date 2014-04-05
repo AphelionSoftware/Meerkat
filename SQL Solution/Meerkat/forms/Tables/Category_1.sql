e90b1e98ec4019c5000cf92ff19c83b14f27c036
MarkGStacey <mark.stacey@aphelion.ws>
2014-04-05 20:21:21 +0300

    Order of questions

--- SQL Solution/Meerkat/forms/Tables/Category_1.sql
+++ SQL Solution/Meerkat/forms/Tables/Category_1.sql
@@ -3,7 +3,8 @@
     [Name]            VARCHAR (50)  NOT NULL,
     [Code]            VARCHAR (50)  NOT NULL,
     [TextDescription] VARCHAR (MAX) NOT NULL,
-    [FormID]          INT           NOT NULL,
+    [Form_ID]         INT           NOT NULL,
+    [CategoryOrder]   INT           NOT NULL,
     [Active]          INT           CONSTRAINT [DF__Category_sys_Active] DEFAULT ((1)) NOT NULL,
     [sys_CreatedBy]   VARCHAR (255) CONSTRAINT [DF_Category_sys_CreatedBy] DEFAULT (user_name()) NOT NULL,
     [sys_CreatedOn]   DATETIME      CONSTRAINT [DF_Category_sys_CreatedOn] DEFAULT (getdate()) NOT NULL,
@@ -11,8 +12,11 @@
     [sys_ModifiedOn]  DATETIME      CONSTRAINT [DF_Category_sys_ModifiedOn] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_Category_3] PRIMARY KEY CLUSTERED ([Category_ID] ASC),
     CONSTRAINT [FK_Category_ActiveType] FOREIGN KEY ([Active]) REFERENCES [Core].[ActiveType] ([ID]),
-    CONSTRAINT [FK_Category_Form] FOREIGN KEY ([FormID]) REFERENCES [forms].[Form] ([Form_ID])
+    CONSTRAINT [FK_Category_Form] FOREIGN KEY ([Form_ID]) REFERENCES [forms].[Form] ([Form_ID]),
+    CONSTRAINT [IX_Category] UNIQUE NONCLUSTERED ([Form_ID] ASC, [CategoryOrder] ASC)
 );
+
+
 
 
 
--- SQL Solution/Meerkat/forms/Tables/Question_1.sql
+++ SQL Solution/Meerkat/forms/Tables/Question_1.sql
@@ -5,6 +5,7 @@
     [TextDescription] VARCHAR (MAX) NOT NULL,
     [QuestionType_ID] INT           NOT NULL,
     [Category_ID]     INT           NOT NULL,
+    [QuestionOrder]   INT           NOT NULL,
     [Active]          INT           CONSTRAINT [DF__Question_sys_Active] DEFAULT ((1)) NOT NULL,
     [sys_CreatedBy]   VARCHAR (255) CONSTRAINT [DF_Question_sys_CreatedBy] DEFAULT (user_name()) NOT NULL,
     [sys_CreatedOn]   DATETIME      CONSTRAINT [DF_Question_sys_CreatedOn] DEFAULT (getdate()) NOT NULL,
@@ -13,8 +14,11 @@
     CONSTRAINT [PK_Question_3] PRIMARY KEY CLUSTERED ([Question_ID] ASC),
     CONSTRAINT [FK_Question_ActiveType] FOREIGN KEY ([Active]) REFERENCES [Core].[ActiveType] ([ID]),
     CONSTRAINT [FK_Question_Category] FOREIGN KEY ([Category_ID]) REFERENCES [forms].[Category] ([Category_ID]),
-    CONSTRAINT [FK_Question_QuestionType] FOREIGN KEY ([QuestionType_ID]) REFERENCES [forms].[QuestionType] ([QuestionType_ID])
+    CONSTRAINT [FK_Question_QuestionType] FOREIGN KEY ([QuestionType_ID]) REFERENCES [forms].[QuestionType] ([QuestionType_ID]),
+    CONSTRAINT [IX_Question] UNIQUE NONCLUSTERED ([Category_ID] ASC, [QuestionOrder] ASC)
 );
+
+
 
 
 
