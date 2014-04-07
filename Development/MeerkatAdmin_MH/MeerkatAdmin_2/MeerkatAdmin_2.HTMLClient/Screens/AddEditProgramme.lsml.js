1d48931ff73208eb67c3f82bf819608011575154
denisngahu <denisngahu@outlook.com>
2014-04-07 19:49:05 +0000

    Created Programme and Frameworks screens

---
+++ Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/Screens/AddEditProgramme.lsml.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.AddEditProgramme.created = function (screen) {
+    // Set default values and other configurations.
+    msls.application.lightswitchTools.configureCaptureForm(screen);
+};
