6fa07daabb063424ccaa4cc75abd7d8b3ef989d4
denisngahu <denisngahu@outlook.com>
2014-04-07 21:17:35 +0000

    Groupd Admin screen added.

---
+++ Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/Screens/AddEditGroup.lsml.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.AddEditGroup.created = function (screen) {
+    // Set default values and other configurations.
+    msls.application.lightswitchTools.configureCaptureForm(screen);
+};
---
+++ Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/Screens/BrowseGroups.lsml.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.BrowseGroups.Home_postRender = function (element, contentItem) {
+    // Include custom home icon.
+    $(element).addClass("customHomeIcon");
+};
