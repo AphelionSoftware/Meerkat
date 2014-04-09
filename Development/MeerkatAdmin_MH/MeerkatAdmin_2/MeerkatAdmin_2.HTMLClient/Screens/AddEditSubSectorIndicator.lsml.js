35c012e8498476fd68ca4bc9f28f75dbbcc5d824
denisngahu <denisngahu@outlook.com>
2014-04-09 15:47:59 +0000

    Sector-Sub Sector screens

---
+++ Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/Screens/AddEditSubSectorIndicator.lsml.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.AddEditSubSectorIndicator.created = function (screen) {
+    // Set default values and other configurations.
+    msls.application.lightswitchTools.configureCaptureForm(screen);
+};
