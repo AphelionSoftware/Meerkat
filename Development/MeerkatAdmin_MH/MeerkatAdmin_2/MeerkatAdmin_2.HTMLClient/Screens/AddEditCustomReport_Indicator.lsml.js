ebf86dd5b5b1b6f5a5246bfadbbdcf6a6f7b2645
denisngahu <denisngahu@outlook.com>
2014-04-14 22:49:50 +0000

    Final admin screen....Aggregated reports

---
+++ Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/Screens/AddEditCustomReport_Indicator.lsml.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.AddEditCustomReport_Indicator.created = function (screen) {
+    // Set default values and other configurations.
+    msls.application.lightswitchTools.configureCaptureForm(screen);
+};
