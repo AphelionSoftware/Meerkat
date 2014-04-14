c34d580bdfea1025bfd545055cb88435102cff20
denisngahu <denisngahu@outlook.com>
2014-04-14 10:30:30 +0000

    Autofill and new fields in capture
    Included code in lightswitchtools.js to autopopulate mandatory sys
    fields and the active type field. Also created browse and add/edit
    screens for project participants reached.

--- Development/MeerkatCapture_MH/Meerkat_Capture/Meerkat_Capture.HTMLClient/Scripts/LightSwitchTools.js
+++ Development/MeerkatCapture_MH/Meerkat_Capture/Meerkat_Capture.HTMLClient/Scripts/LightSwitchTools.js
@@ -57,6 +57,103 @@
             callback("version: " + data.Version + " (built " + data.Deployed + ")");
         });
     }
+
+    lightswitchTools.configureCaptureForm = function (screen) {
+        var name = screen.details.getModel().properties[0].name;
+
+        var primaryKeyColumn;
+        var properties = screen.details.getModel().properties[0].elementType.properties;
+
+        $.each(properties, function (index, property) {
+            if (property.__isKeyProperty === 1) {
+                primaryKeyColumn = property.name;
+                return false;
+            }
+        });
+
+        var thisObject = screen[name];
+        var primaryKey = thisObject[primaryKeyColumn];
+        var activeType = thisObject.ActiveType;
+
+        if (primaryKey === undefined) {
+            if (screen.pageTitle !== undefined) {
+                screen.details.displayName = "Add " + screen.pageTitle;
+            } else {
+                screen.details.displayName = "Add " + name;
+            }
+
+            thisObject.sys_CreatedBy = "NA";
+            thisObject.sys_CreatedOn = "1999/01/01";
+            thisObject.sys_ModifiedBy = "NA";
+            thisObject.sys_ModifiedOn = "1999/01/01";
+
+            var activeTypeDropDown = screen.findContentItem("ActiveType");
+
+            if (activeTypeDropDown !== undefined) {
+                activeTypeDropDown.isVisible = false;
+            }
+
+            screen.details.dataWorkspace.MeerkatData.ActiveTypes.filter("Code eq 'Active'").execute().then(function (x) {
+                thisObject.setActiveType(x.results[0]);
+            }, function (x) {
+                msls.showMessageBox(x, {
+                    title: "Default value for ActiveType failed"
+                });
+            });
+
+
+            return;
+        } else {
+            if (screen.pageTitle !== undefined) {
+                screen.details.displayName = "Edit " + screen.pageTitle;
+            } else {
+                screen.details.displayName = "Edit " + name;
+            }
+
+            var newDataWorkspace = new myapp.DataWorkspace();
+            newDataWorkspace.MeerkatData[pluralName(name)].filter(primaryKeyColumn + " eq " + primaryKey).execute().then(function (result) {
+
+                var serverValue = result.results[0];
+                if (serverValue !== undefined) {
+                    var detailsProperties = thisObject.details.properties.all();
+                    var foundChanges = false;
+                    $.each(detailsProperties, function (index, property) {
+                        var propertyName = property.name;
+
+                        if ((property instanceof msls.Entity.Details.StorageProperty) &&
+                            (propertyName[0] !== '_') &&
+                            (propertyName.substring(0, 3) !== 'sys')) {
+                            var diffFound = false;
+
+                            if (thisObject.details._[propertyName] instanceof Date) {
+                                diffFound = (thisObject.details._[propertyName].toString() !== serverValue.details._[propertyName].toString());
+                            } else {
+                                diffFound = (thisObject.details._[propertyName] !== serverValue.details._[propertyName]);
+                            }
+
+                            if (diffFound) {
+                                // we found a real property that has different values.
+                                console.log("Detected that " + propertyName + " has changed");
+                                thisObject.details._[propertyName] = serverValue.details._[propertyName];
+                                property.dispatchChange("value");
+                                thisObject.dispatchChange(propertyName);
+                                foundChanges = true;
+                            }
+                        }
+                    });
+
+                    if (foundChanges) {
+                        thisObject.details._.__metadata.etag = serverValue.details._.__metadata.etag;
+                    }
+                }
+            }, function (error) {
+                msls.showMessageBox(error, {
+                    title: "Data Refresh failed"
+                });
+            });
+        }
+
+    }
     
 }(msls.application.lightswitchTools = msls.application.lightswitchTools || {}));
 
