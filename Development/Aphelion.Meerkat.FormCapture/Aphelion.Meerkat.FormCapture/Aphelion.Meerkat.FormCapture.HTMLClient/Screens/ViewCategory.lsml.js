333642b0cf9ca901a4c7076a16c69ee7ffbd0ea6
MarkGStacey <mark.stacey@aphelion.ws>
2014-05-04 14:55:55 +0200

    Form capture updates

---
+++ Development/Aphelion.Meerkat.FormCapture/Aphelion.Meerkat.FormCapture/Aphelion.Meerkat.FormCapture.HTMLClient/Screens/ViewCategory.lsml.js
@@ -0,0 +1,53 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewCategory.RowTemplate_postRender = function (element, contentItem) {
+    // Write code here.
+    var x = contentItem.data;
+    };
+myapp.ViewCategory.QuestionType_postRender = function (element, contentItem) {
+    // Write code here.
+
+    /*
+    //Capture example
+    contentItem.dataBind("value", function (newValue) {
+
+        var indicatorGroup = contentItem.screen.findContentItem("SubIndicators");
+        $.each(indicatorGroup.children, function (index, child) {
+            child.isVisible = false;
+        });
+
+        if (newValue === undefined) {
+            return;
+        }
+
+        var target = undefined;
+        switch (newValue.Code) {
+            case "Outcome":
+                {
+                    target = contentItem.screen.findContentItem("OutcomeGroup");
+                    break;
+                }
+            case "Output":
+                {
+                    target = contentItem.screen.findContentItem("OutputGroup");
+                    break;
+                }
+            case "SubOutput":
+                {
+                    target = contentItem.screen.findContentItem("SubOutpuGroupt");
+                    break;
+                }
+            default:
+                {
+                    console.log(newValue.Code);
+                    break;
+                }
+
+        }
+
+        if (target !== undefined) {
+            target.isVisible = true;
+        }
+    });
+    */
+};
