2eb0dfd15ad24796726e0a80593018cbb4f1dd6e
denisngahu <denisngahu@outlook.com>
2014-05-15 14:41:37 +0300

    Meerkat Admin Screen fixes.

--- Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/Screens/AddEditDataSource.lsml.js
+++ Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/Screens/AddEditDataSource.lsml.js
@@ -1,12 +1,12 @@
 ﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
 /// <reference path="LightSwitchTools.js" />
 
+myapp.AddEditDataSource.created = function (screen) {
+    msls.application.lightswitchTools.configureCaptureForm(screen);
+};
 myapp.AddEditDataSource.DataSource_Delete_execute = function (screen) {
     msls.application.lightswitchTools.deleteEntity(screen);
 };
 myapp.AddEditDataSource.DataSource_Delete_canExecute = function (screen) {
     return msls.application.lightswitchTools.canDelete(screen);
-};
-myapp.AddEditDataSource.created = function (screen) {
-    msls.application.lightswitchTools.configureCaptureForm(screen);
 };
