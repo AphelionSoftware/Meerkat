fc50a1cc897e5a56527bdc9e8ea53eae4ccaa635
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-06-11 03:32:38 +0000

    Updates to FormBuilder and Capture

--- Development/Aphelion.Meerkat.FormCapture/Aphelion.Meerkat.FormCapture/Aphelion.Meerkat.FormCapture.HTMLClient/Screens/AddEditFormResponse.lsml.js
+++ Development/Aphelion.Meerkat.FormCapture/Aphelion.Meerkat.FormCapture/Aphelion.Meerkat.FormCapture.HTMLClient/Screens/AddEditFormResponse.lsml.js
@@ -3,28 +3,32 @@
 myapp.AddEditFormResponse.RowTemplate1_postRender = function (element, contentItem) {
     // Write code here.
 
+   
+};
+myapp.AddEditFormResponse.Responses1Template_postRender = function (element, contentItem) {
+    // Write code here.
     var type = contentItem.data.Question.QuestionType.Code;
     var x = element;
 
+    contentItem.children[1].isVisible = false;
     contentItem.children[2].isVisible = false;
     contentItem.children[3].isVisible = false;
-    contentItem.children[4].isVisible = false;
     switch (type) {
         case "TR":
             {
-                contentItem.children[2].isVisible = true;
+                contentItem.children[1].isVisible = true;
                 break;
             }
         case "TF":
         case "BOOL":
             {
-                contentItem.children[3].isVisible = true;
+                contentItem.children[2].isVisible = true;
                 break;
             }
         case "RB":
         case "MCQ":
             {
-                contentItem.children[4].isVisible = true;
+                contentItem.children[3].isVisible = true;
                 break;
             }
 
