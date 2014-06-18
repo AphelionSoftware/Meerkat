cbc20ac8a8f589fec5fa64d1c7088db68142c080
denisngahu <denisngahu@outlook.com>
2014-06-18 13:22:59 +0300

    Capture rollup changes

---
+++ Development/MeerkatCapture_MH/Meerkat_Capture/Meerkat_Capture.HTMLClient/Screens/AddEditMilestoneValue.lsml.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.AddEditMilestoneValue.created = function (screen) {
+    // Write code here.
+    msls.application.lightswitchTools.configureCaptureForm(screen);
+};
