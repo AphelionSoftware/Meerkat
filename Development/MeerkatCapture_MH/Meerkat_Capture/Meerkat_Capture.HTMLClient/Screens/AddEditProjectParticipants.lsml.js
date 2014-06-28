947f3b1b439c96827a68bdde7b22a562bbce3000
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-06-28 16:16:27 +0000

    Fixed Location edit - child location edit still to be fixerd

--- Development/MeerkatCapture_MH/Meerkat_Capture/Meerkat_Capture.HTMLClient/Screens/AddEditProjectParticipants.lsml.js
+++ Development/MeerkatCapture_MH/Meerkat_Capture/Meerkat_Capture.HTMLClient/Screens/AddEditProjectParticipants.lsml.js
@@ -3,4 +3,15 @@
 myapp.AddEditProjectParticipants.created = function (screen) {
     // Write code here.
     msls.application.lightswitchTools.configureCaptureForm(screen);
+
+
+    //This should really be done at the data source level
+    $.getJSON("/api/TodaysReportingPeriod", function (data) {
+        myapp.activeDataWorkspace.MeerkatData.ReportingPeriods_SingleOrDefault(data).execute().then(function (reportingPeriod) {
+            screen.MaxReportingRangeID = reportingPeriod.results[0].EndDateID;
+            screen.ReportingPeriodsFiltered.load().then(function () {
+                screen.PeopleReachedValue.setReportingPeriod(reportingPeriod.results[0]);
+            });
+        });
+    });
 };
