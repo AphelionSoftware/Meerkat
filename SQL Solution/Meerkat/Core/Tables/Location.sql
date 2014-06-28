6326d94b16ea3de2d98e2c665fff91453445b2d9
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-06-28 16:23:03 +0000

    Location edits fixed for Admin

--- SQL Solution/Meerkat/Core/Tables/Location.sql
+++ SQL Solution/Meerkat/Core/Tables/Location.sql
@@ -2,10 +2,10 @@
     [Location_ID]       INT               IDENTITY (1, 1) NOT NULL,
     [Code]              VARCHAR (50)      NOT NULL,
     [Name]              VARCHAR (255)     NOT NULL,
-    [AreaKM]              DECIMAL(18, 2)     NULL,
-    [Population]              DECIMAL(18, 2)     NULL,
-	[Density]           VARCHAR (MAX)     NULL,
-	[BusinessKey]       NVARCHAR (4000)    NOT NULL,
+    [AreaKM]            DECIMAL (18, 2)   NULL,
+    [Population]        DECIMAL (18, 2)   NULL,
+    [Density]           VARCHAR (MAX)     NULL,
+    [BusinessKey]       NVARCHAR (4000)   NOT NULL,
     [LocationType_ID]   INT               NULL,
     [ParentLocation_ID] INT               NULL,
     [Geog]              [sys].[geography] NULL,
@@ -18,9 +18,11 @@
     CONSTRAINT [FK_Location_ActiveType] FOREIGN KEY ([Active]) REFERENCES [Core].[ActiveType] ([ID]),
     CONSTRAINT [FK_Location_LocationType] FOREIGN KEY ([LocationType_ID]) REFERENCES [Core].[LocationType] ([LocationType_ID]),
     CONSTRAINT [FK_Location_ParentLocation] FOREIGN KEY ([ParentLocation_ID]) REFERENCES [Core].[Location] ([Location_ID]),
-    CONSTRAINT [UQ_Location_Code] UNIQUE NONCLUSTERED ([Code] ASC),
-    CONSTRAINT [UQ_Location_Name]  UNIQUE NONCLUSTERED ([Name] ASC)
+    CONSTRAINT [UQ_Location_BusinessKey] UNIQUE NONCLUSTERED ([BusinessKey] ASC),
+    CONSTRAINT [UQ_Location_Code] UNIQUE NONCLUSTERED ([ParentLocation_ID] ASC, [Code] ASC)
 );
+
+
 
 
 
