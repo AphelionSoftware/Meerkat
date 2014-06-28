1df07cdcbaffc613ae4626239c2a7c65345deb0e
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-06-28 16:56:34 +0000

    Final admin changes

--- Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.Server/DataSources/MeerkatData/_MeerkatDataService.lsml.cs
+++ Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.Server/DataSources/MeerkatData/_MeerkatDataService.lsml.cs
@@ -298,147 +298,147 @@ namespace LightSwitchApplication
         partial void Outcomes_Filter(ref Expression<Func<Outcome, bool>> filter)
         {
             // filter = e => e.IntegerProperty == 0;
-            filter = e => (e.ActiveType.ID == 1);
+            filter = e => (e.ActiveType.ID >= 0 );
         }
 
         partial void Outputs_Filter(ref Expression<Func<Output, bool>> filter)
         {
-            filter = e => (e.ActiveType.ID == 1);
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void Activities_Filter(ref Expression<Func<Activity, bool>> filter)
         {
             // filter = e => e.IntegerProperty == 0;
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
        
         partial void DataVersions_Filter(ref Expression<Func<DataVersion, bool>> filter)
         {
             // filter = e => e.IntegerProperty == 0;
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void IndicatorLocations_Filter(ref Expression<Func<IndicatorLocation, bool>> filter)
         {
             // filter = e => e.IntegerProperty == 0;
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void Indicators_Filter(ref Expression<Func<Indicator, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void IndicatorTypes_Filter(ref Expression<Func<IndicatorType, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void IndicatorValues_Filter(ref Expression<Func<IndicatorValue, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void Locations_Filter(ref Expression<Func<Location, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void LocationTypes_Filter(ref Expression<Func<LocationType, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void MilestoneLocations_Filter(ref Expression<Func<MilestoneLocation, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void Milestones_Filter(ref Expression<Func<Milestone, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void MilestoneTypes_Filter(ref Expression<Func<MilestoneType, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void MilestoneValues_Filter(ref Expression<Func<MilestoneValue, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void Organizations_Filter(ref Expression<Func<Organization, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void OrganizationTypes_Filter(ref Expression<Func<OrganizationType, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void OutcomeOrganizations_Filter(ref Expression<Func<OutcomeOrganization, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void OutcomePersonRoles_Filter(ref Expression<Func<OutcomePersonRole, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void OutputPersonRoles_Filter(ref Expression<Func<OutputPersonRole, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void People_Filter(ref Expression<Func<Person, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void Projects_Filter(ref Expression<Func<Project, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void ReportingPeriods_Filter(ref Expression<Func<ReportingPeriod, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void Roles_Filter(ref Expression<Func<Role, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
         partial void SubOutputs_Filter(ref Expression<Func<SubOutput, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
 
         }
 
@@ -480,12 +480,12 @@ namespace LightSwitchApplication
 
         partial void StatusValues_Filter(ref Expression<Func<StatusValue, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
         }
 
         partial void StatusTypes_Filter(ref Expression<Func<StatusType, bool>> filter)
         {
-            filter = e => e.ActiveType.ID == 1;
+            filter = e => (e.ActiveType.ID >= 0 );
         }
     }
 }
