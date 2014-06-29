e325710fdf75bb00fd69e0d0f03158bcd38bd03d
denisngahu <denisngahu@outlook.com>
2014-06-29 13:19:54 +0300

    Data source edits - show by default

---
+++ Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/Screens/AddEditFramework.lsml.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+myapp.AddEditFramework.created = function (screen) {
+    // Write code here.
+    msls.application.lightswitchTools.configureCaptureForm(screen);
+};
