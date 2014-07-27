da94d0ebf4c2be28188b24d8dfe4a6cc3dd6211a
MarkGStacey <mark.stacey@aphelion.ws>
2014-07-27 10:22:54 +0000

    Custom reports module

---
+++ Development/Meerkat.ReportsManager/Meerkat.ReportsManager/Meerkat.ReportsManager.HTMLClient/Screens/CustomReportTypes/ViewCustomReportType.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewCustomReportType.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.CustomReportType.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.CustomReportType." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
---
+++ Development/Meerkat.ReportsManager/Meerkat.ReportsManager/Meerkat.ReportsManager.HTMLClient/Screens/CustomReports/AddEditCustomReport.lsml.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+/// <reference path="../Scripts/LightSwitchTools.js" />
+
+myapp.AddEditCustomReport.created = function (screen) {
+    msls.application.lightswitchTools.configureForm(screen);
+
+};
+myapp.AddEditCustomReport.Code_postRender = function (element, contentItem) {
+    /*contentItem.dataBind("screen.Code.value", function (element, contentItem) {
+        contentItem.screen.findContentItem("BusinessKey").value = contentItem.value;
+    }
+    );*/
+
+};
