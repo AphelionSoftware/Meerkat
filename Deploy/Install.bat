42c1b68f52b2abec25a60272b3298572207bf162
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-07-31 12:30:33 +0000

    IISRESET

--- Deploy/Install.bat
+++ Deploy/Install.bat
@@ -3,3 +3,4 @@ Meerkat_Admin\Meerkat_Admin.deploy.cmd /Y
 Meerkat_FormCapture\Meerkat_FormCapture.deploy.cmd /Y
 Meerkat_FormBuilder\Meerkat_FormBuilder.deploy.cmd /Y
 Meerkat_EventRegister\Meerkat_EventRegister.deploy.cmd /Y
+IISRESET /restart
