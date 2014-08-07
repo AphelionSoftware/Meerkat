4fcac557f20468b517618140c78dd199e364338a
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-08-07 08:24:11 +0000

    Build update

--- Build.bat
+++ Build.bat
@@ -1,5 +1,5 @@
 "C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\msbuild.exe" "Development\MeerkatCapture_MH\Meerkat_Capture\Meerkat_Capture.lsxproj" /T:Publish /P:Configuration=Release
-"C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\msbuild.exe" "Development\MeerkatAdmin_MH\MeerkatAdmin_2\MeerkatAdmin_2_1" /T:Publish /P:Configuration=Release
+"C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\msbuild.exe" "Development\MeerkatAdmin_MH\MeerkatAdmin_2\MeerkatAdmin_2_1.lsxproj" /T:Publish /P:Configuration=Release
 "C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\msbuild.exe" "Development\Aphelion.Meerkat.FormBuilder\Aphelion.Meerkat.FormBuilder.lsxproj" /T:Publish /P:Configuration=Release
 "C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\msbuild.exe" "Development\Aphelion.Meerkat.FormCapture\Aphelion.Meerkat.FormCapture\Aphelion.Meerkat.FormCapture.lsxproj" /T:Publish /P:Configuration=Release
 "C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\msbuild.exe" "Development\Aphelion.Meerkat.EventRegister\Aphelion.Meerkat.EventRegister\Aphelion.Meerkat.EventRegister.lsxproj" /T:Publish /P:Configuration=Release
