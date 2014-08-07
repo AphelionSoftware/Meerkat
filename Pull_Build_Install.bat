48aab6763f8c7f7376d4233d214859728b7f36c9
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-08-07 08:34:49 +0000

    Change to builds

--- Pull_Build_Install.bat
+++ Pull_Build_Install.bat
@@ -1,2 +1,4 @@
-"C:\Program Files (x86)\Git\bin\git.exe" fetch origin/r02_CARE_Screens/Deployment
-"C:\Program Files (x86)\Git\bin\git.exe" checkout r02_CARE_Screens/Deployment
+"C:\Program Files (x86)\Git\bin\git.exe" fetch --progress "origin" +refs/heads/r02_CARE_Screens/Deployment:refs/remotes/origin/r02_CARE_Screens/Deployment
+"C:\Program Files (x86)\Git\bin\git.exe" pull "origin" +refs/heads/r02_CARE_Screens/Deployment:refs/remotes/origin/r02_CARE_Screens/Deployment
+Build.bat
+Deploy\Install.bat
