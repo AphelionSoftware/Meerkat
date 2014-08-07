1934f469418eb77e171ea6f684a75715462f5ba0
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-08-07 08:37:02 +0000

    Build fix

--- Build.bat
+++ Build.bat
@@ -5,10 +5,10 @@
 "C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\Aphelion.Meerkat.EventRegister\Aphelion.Meerkat.EventRegister\Aphelion.Meerkat.EventRegister.lsxproj" /T:Publish /P:Configuration=Release
 "C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\Meerkat.ReportsManager\Meerkat.ReportsManager\Meerkat.ReportsManager.lsxproj" /T:Publish /P:Configuration=Release
 
-xcopy c:\installs\Meerkat_Capture\*.*  %~dp0\DeployMeerkat_Capture /A
-xcopy c:\installs\Meerkat_Admin\*.*  %~dp0\DeployMeerkat_Admin /A
-xcopy c:\installs\Meerkat_FormBuilder\*.*  %~dp0\Deploy\Meerkat_FormBuilder /A
-xcopy c:\installs\Meerkat_FormCapture\*.*  %~dp0\Deploy\Meerkat_FormCapture /A
-xcopy c:\installs\Meerkat_EventRegister\*.*  %~dp0\Deploy\Meerkat_EventRegister /a
-xcopy c:\installs\Meerkat_ReportsManager\*.*  %~dp0\Deploy\Meerkat_ReportsManager /a
+xcopy c:\installs\Meerkat_Capture\*.*  %~dp0\DeployMeerkat_Capture\ /A
+xcopy c:\installs\Meerkat_Admin\*.*  %~dp0\DeployMeerkat_Admin\ /A
+xcopy c:\installs\Meerkat_FormBuilder\*.*  %~dp0\Deploy\Meerkat_FormBuilder\ /A
+xcopy c:\installs\Meerkat_FormCapture\*.*  %~dp0\Deploy\Meerkat_FormCapture\ /A
+xcopy c:\installs\Meerkat_EventRegister\*.*  %~dp0\Deploy\Meerkat_EventRegister\ /A
+xcopy c:\installs\Meerkat_ReportsManager\*.*  %~dp0\Deploy\Meerkat_ReportsManager\ /A
 
