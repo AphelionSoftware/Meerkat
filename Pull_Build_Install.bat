8256e4351e83a4c50c305386916357344c75af01
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-08-07 09:13:10 +0000

    Reset

--- Pull_Build_Install.bat
+++ Pull_Build_Install.bat
@@ -1,4 +1,5 @@
 "C:\Program Files (x86)\Git\bin\git.exe" fetch --progress "origin" +refs/heads/r02_CARE_Screens/Deployment:refs/remotes/origin/r02_CARE_Screens/Deployment
+git reset --hard HEAD
 git clean -f -d
 "C:\Program Files (x86)\Git\bin\git.exe" pull "origin" +refs/heads/r02_CARE_Screens/Deployment:refs/remotes/origin/r02_CARE_Screens/Deployment
 Build.bat
