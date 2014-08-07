0100e8810d2362e583ba93d37f243937ceef4f27
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-08-07 09:21:48 +0000

    CALL batches

--- Install.bat
+++ Install.bat
@@ -1,7 +1,7 @@
-Deploy\Meerkat_Capture\Meerkat_Capture.deploy.cmd /Y
-Deploy\Meerkat_Admin\Meerkat_Admin.deploy.cmd /Y
-Deploy\Meerkat_FormCapture\Aphelion.Meerkat.FormCapture.deploy.cmd /Y
-Deploy\Meerkat_FormBuilder\Aphelion.Meerkat.FormBuilder.deploy.cmd /Y
-Deploy\Meerkat_EventRegister\Aphelion.Meerkat.EventRegister.deploy.cmd /Y
-Deploy\Meerkat_ReportsManager\Meerkat.ReportsManager.deploy.cmd /Y
+CALL Deploy\Meerkat_Capture\Meerkat_Capture.deploy.cmd /Y
+CALL Deploy\Meerkat_Admin\Meerkat_Admin.deploy.cmd /Y
+CALL Deploy\Meerkat_FormCapture\Aphelion.Meerkat.FormCapture.deploy.cmd /Y
+CALL Deploy\Meerkat_FormBuilder\Aphelion.Meerkat.FormBuilder.deploy.cmd /Y
+CALL Deploy\Meerkat_EventRegister\Aphelion.Meerkat.EventRegister.deploy.cmd /Y
+CALL Deploy\Meerkat_ReportsManager\Meerkat.ReportsManager.deploy.cmd /Y
 IISRESET /restart
