e2fc32ddb74ff31a6259f068dd97c69a583df614
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-08-07 08:32:43 +0000

    Build changes

--- Build.bat
+++ Build.bat
@@ -1,9 +1,9 @@
-"C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\msbuild.exe" "Development\MeerkatCapture_MH\Meerkat_Capture\Meerkat_Capture.lsxproj" /T:Publish /P:Configuration=Release
-"C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\msbuild.exe" "Development\MeerkatAdmin_MH\MeerkatAdmin_2\MeerkatAdmin_2_1.lsxproj" /T:Publish /P:Configuration=Release
-"C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\msbuild.exe" "Development\Aphelion.Meerkat.FormBuilder\Aphelion.Meerkat.FormBuilder.lsxproj" /T:Publish /P:Configuration=Release
-"C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\msbuild.exe" "Development\Aphelion.Meerkat.FormCapture\Aphelion.Meerkat.FormCapture\Aphelion.Meerkat.FormCapture.lsxproj" /T:Publish /P:Configuration=Release
-"C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\msbuild.exe" "Development\Aphelion.Meerkat.EventRegister\Aphelion.Meerkat.EventRegister\Aphelion.Meerkat.EventRegister.lsxproj" /T:Publish /P:Configuration=Release
-"C:\Program Files (x86)\MSBuild\12.0\Bin\amd64\msbuild.exe" "Development\Meerkat.ReportsManager\Meerkat.ReportsManager\Meerkat.ReportsManager.lsxproj" /T:Publish /P:Configuration=Release
+"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\MeerkatCapture_MH\Meerkat_Capture\Meerkat_Capture.lsxproj" /T:Publish /P:Configuration=Release
+"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\MeerkatAdmin_MH\MeerkatAdmin_2\MeerkatAdmin_2_1.lsxproj" /T:Publish /P:Configuration=Release
+"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\Aphelion.Meerkat.FormBuilder\Aphelion.Meerkat.FormBuilder.lsxproj" /T:Publish /P:Configuration=Release
+"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\Aphelion.Meerkat.FormCapture\Aphelion.Meerkat.FormCapture\Aphelion.Meerkat.FormCapture.lsxproj" /T:Publish /P:Configuration=Release
+"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\Aphelion.Meerkat.EventRegister\Aphelion.Meerkat.EventRegister\Aphelion.Meerkat.EventRegister.lsxproj" /T:Publish /P:Configuration=Release
+"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\Meerkat.ReportsManager\Meerkat.ReportsManager\Meerkat.ReportsManager.lsxproj" /T:Publish /P:Configuration=Release
 
 xcopy c:\installs\Meerkat_Capture\*.*  %~dp0\DeployMeerkat_Capture /A
 xcopy c:\installs\Meerkat_Admin\*.*  %~dp0\DeployMeerkat_Admin /A
