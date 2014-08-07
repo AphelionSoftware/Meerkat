38ad753e650f36597d51db7c3e109f710ba815ae
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-08-07 09:02:52 +0000

    Installs

--- Install.bat
+++ Install.bat
@@ -1,7 +1,7 @@
-\Deploy\Meerkat_Capture\Meerkat_Capture.deploy.cmd /Y
-\Deploy\Meerkat_Admin\Meerkat_Admin.deploy.cmd /Y
-\Deploy\Meerkat_FormCapture\Meerkat_FormCapture.deploy.cmd /Y
-\Deploy\Meerkat_FormBuilder\Meerkat_FormBuilder.deploy.cmd /Y
-\Deploy\Meerkat_EventRegister\Meerkat_EventRegister.deploy.cmd /Y
-\Deploy\Meerkat_ReportsManager\Meerkat_ReportsManager.deploy.cmd /Y
+Deploy\Meerkat_Capture\Meerkat_Capture.deploy.cmd /Y
+Deploy\Meerkat_Admin\Meerkat_Admin.deploy.cmd /Y
+Deploy\Meerkat_FormCapture\Aphelion.Meerkat.FormCapture.deploy.cmd /Y
+Deploy\Meerkat_FormBuilder\Aphelion.Meerkat.FormBuilder.deploy.cmd /Y
+Deploy\Meerkat_EventRegister\Aphelion.Meerkat.EventRegister.deploy.cmd /Y
+Deploy\Meerkat_ReportsManager\Meerkat.ReportsManager.deploy.cmd /Y
 IISRESET /restart
--- Pull_Build_Install.bat
+++ Pull_Build_Install.bat
@@ -1,4 +1,4 @@
 "C:\Program Files (x86)\Git\bin\git.exe" fetch --progress "origin" +refs/heads/r02_CARE_Screens/Deployment:refs/remotes/origin/r02_CARE_Screens/Deployment
 "C:\Program Files (x86)\Git\bin\git.exe" pull "origin" +refs/heads/r02_CARE_Screens/Deployment:refs/remotes/origin/r02_CARE_Screens/Deployment
 Build.bat
-Deploy\Install.bat
+Install.bat
