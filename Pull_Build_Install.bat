43a95a1cf3ac817ff4b5ea5384e8609043b80ce0
MarkGStacey <mark.stacey@aphelion.ws>
2014-08-07 13:23:00 +0000

    PUll ito separate file

--- Pull_Build_Install.bat
+++ Pull_Build_Install.bat
@@ -1,6 +1,3 @@
-"C:\Program Files (x86)\Git\bin\git.exe" fetch --progress "origin" +refs/heads/r02_CARE_Screens/Deployment:refs/remotes/origin/r02_CARE_Screens/Deployment
-git reset --hard HEAD
-git clean -f -d
-"C:\Program Files (x86)\Git\bin\git.exe" pull "origin" +refs/heads/r02_CARE_Screens/Deployment:refs/remotes/origin/r02_CARE_Screens/Deployment
+CALL Pull.bat
 CALL Build.bat
 CALL Install.bat
