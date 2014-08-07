faca5d1602953c55524ef937564064b853813c6f
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-08-07 08:54:12 +0000

    Installs to root

--- Deploy/Install.bat
+++
@@ -1,7 +0,0 @@
-Meerkat_Capture\Meerkat_Capture.deploy.cmd /Y
-Meerkat_Admin\Meerkat_Admin.deploy.cmd /Y
-Meerkat_FormCapture\Meerkat_FormCapture.deploy.cmd /Y
-Meerkat_FormBuilder\Meerkat_FormBuilder.deploy.cmd /Y
-Meerkat_EventRegister\Meerkat_EventRegister.deploy.cmd /Y
-Meerkat_ReportManager\Meerkat_ReportManager.deploy.cmd /Y
-IISRESET /restart
---
+++ Install.bat
@@ -0,0 +1,7 @@
+\Deploy\Meerkat_Capture\Meerkat_Capture.deploy.cmd /Y
+\Deploy\Meerkat_Admin\Meerkat_Admin.deploy.cmd /Y
+\Deploy\Meerkat_FormCapture\Meerkat_FormCapture.deploy.cmd /Y
+\Deploy\Meerkat_FormBuilder\Meerkat_FormBuilder.deploy.cmd /Y
+\Deploy\Meerkat_EventRegister\Meerkat_EventRegister.deploy.cmd /Y
+\Deploy\Meerkat_ReportsManager\Meerkat_ReportsManager.deploy.cmd /Y
+IISRESET /restart
