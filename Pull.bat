160c2cbef45e33de37b3a920ff700c1523d4faef
MarkGStacey <mark.stacey@aphelion.ws>
2014-08-07 13:39:34 +0000

    Force checkout

--- Pull.bat
+++ Pull.bat
@@ -2,3 +2,4 @@
 git reset --hard HEAD
 git clean -f -d
 "C:\Program Files (x86)\Git\bin\git.exe" pull "origin" +refs/heads/r02_CARE_Screens/Deployment:refs/remotes/origin/r02_CARE_Screens/Deployment
+"C:\Program Files (x86)\Git\bin\git.exe" checkout --force -B "r02_CARE_Screens/Deployment" "origin/r02_CARE_Screens/Deployment"
