131873662b96f52821ed21d50de9680c268b1e03
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-08-10 09:49:08 +0000

    Results to Deploy folder
    (cherry picked from commit 402c6c935d5706f40f734d7fc80097e0e579c46f)

--- Install.bat
+++ Install.bat
@@ -1,7 +1,7 @@
-CALL Deploy\Meerkat_Capture\Meerkat_Capture.deploy.cmd /Y
-CALL Deploy\Meerkat_Admin\Meerkat_Admin.deploy.cmd /Y
-CALL Deploy\Meerkat_FormCapture\Aphelion.Meerkat.FormCapture.deploy.cmd /Y
-CALL Deploy\Meerkat_FormBuilder\Aphelion.Meerkat.FormBuilder.deploy.cmd /Y
-CALL Deploy\Meerkat_EventRegister\Aphelion.Meerkat.EventRegister.deploy.cmd /Y
-CALL Deploy\Meerkat_ReportsManager\Meerkat.ReportsManager.deploy.cmd /Y
+CALL Deploy\Meerkat_Capture\Meerkat_Capture.deploy.cmd /Y  > Deploy\Meerkat_CaptureLS.txt
+CALL Deploy\Meerkat_Admin\Meerkat_Admin.deploy.cmd /Y  > Deploy\Meerkat_AdminLS.txt
+CALL Deploy\Meerkat_FormCapture\Aphelion.Meerkat.FormCapture.deploy.cmd /Y  > Deploy\Meerkat_FormCaptureLS.txt
+CALL Deploy\Meerkat_FormBuilder\Aphelion.Meerkat.FormBuilder.deploy.cmd /Y  > Deploy\Meerkat_FormBuilderLS.txt
+CALL Deploy\Meerkat_EventRegister\Aphelion.Meerkat.EventRegister.deploy.cmd /Y  > Deploy\Meerkat_EventRegisterLS.txt
+CALL Deploy\Meerkat_ReportsManager\Meerkat.ReportsManager.deploy.cmd /Y  > Deploy\Meerkat_ReportsManagerLS.txt
 IISRESET /restart
