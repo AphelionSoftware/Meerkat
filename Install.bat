3d77315c8374e4fcfb52149b0adf3970e8009d13
MarkGStacey <mark.stacey@aphelion.ws>
2014-08-11 11:04:18 +0000

    News Agg copy file
    (cherry picked from commit 08809a7896e356ab7ea52274c4dd10e1a4b3d6f3)

--- Install.bat
+++ Install.bat
@@ -1,3 +1,4 @@
 CALL powershell "& 'Installation\Install_SSIS.ps1'"
 C:\Program Files\7-Zip\7z.exe" x Installation\MegaMenu.7z -o"C:\inetpub\Meerkat_Megamenu" -y
+C:\Program Files\7-Zip\7z.exe" x Installation\NewsAgg.7z -o"C:\inetpub\Meerkat_NewsAgg" -y
 CALL powershell "& 'Installation\Install_LSWebApps.ps1'"
