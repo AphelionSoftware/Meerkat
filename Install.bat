d2add8f33620fbb2ce21b6ad1047fa2c957a7227
MarkGStacey <mark.stacey@aphelion.ws>
2014-08-11 12:18:33 +0000

    Creating SSIS security and deploying jobs

--- Install.bat
+++ Install.bat
@@ -1,4 +1,5 @@
 CALL powershell "& 'Installation\Install_SSIS.ps1'"
+CALL powershell "& 'Installation\SetupProxies.ps1'"
 C:\Program Files\7-Zip\7z.exe" x Installation\MegaMenu.7z -o"C:\inetpub\Meerkat_Megamenu" -y
 C:\Program Files\7-Zip\7z.exe" x Installation\NewsAgg.7z -o"C:\inetpub\Meerkat_NewsAgg" -y
 CALL powershell "& 'Installation\Install_LSWebApps.ps1'"
--- Install_Build_Deploy.bat
+++ Install_Build_Deploy.bat
@@ -1,3 +1,4 @@
 CALL Install.bat
 CALL Build.bat
 CALL Deploy.bat
+CALL powershell "& 'Installation\Create_SSISJobs.ps1'"
