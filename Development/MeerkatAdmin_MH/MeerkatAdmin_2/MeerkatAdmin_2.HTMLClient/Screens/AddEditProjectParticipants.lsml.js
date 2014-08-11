99ee795a40410ced4a9c90fe6f9a3b92077a966c
denisngahu <denisngahu@outlook.com>
2014-08-11 17:25:01 +0300

    Admin defaults and Database changes
    (cherry picked from commit 1afc29f67e1efc14b65a48dda294e02b32c97e2e)
    
    Conflicts:
    	Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/Properties/Proxies.lsml
    	Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.Server/ModelManifest.xml
    	Development/MeerkatCapture_MH/Meerkat_Capture/Meerkat_Capture.Server/ModelManifest.xml
    	SQL Solution/Aphelion.Meerkat.DB/Meerkat/Aphelion.Meerkat.DB.sqlproj

--- Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/Screens/AddEditProjectParticipants.lsml.js
+++ Development/MeerkatAdmin_MH/MeerkatAdmin_2/MeerkatAdmin_2.HTMLClient/Screens/AddEditProjectParticipants.lsml.js
@@ -4,7 +4,7 @@
 myapp.AddEditProjectParticipants.created = function (screen) {
     // Set default values and other configurations.
     msls.application.lightswitchTools.configureCaptureForm(screen);
-    //msls.application.lightswitchTools.setBusinessKeyIsName(screen);
+    msls.application.lightswitchTools.setBusinessKeyIsName(screen);
 };
 myapp.AddEditProjectParticipants.beforeApplyChanges = function (screen) {
     // Write code here.
