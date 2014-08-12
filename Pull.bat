5bfe593958a3796f3f38ed5ac013c500225cc38c
MarkGStacey <mark.stacey@aphelion.ws>
2014-08-12 06:39:03 +0000

    Fix Pull

--- Pull.bat
+++ Pull.bat
@@ -1,5 +1,5 @@
-"C:\Program Files (x86)\Git\bin\git.exe" fetch --progress "origin" +refs/heads/r02_CARE_Screens/Deployment:refs/remotes/origin/r02_CARE_Screens/Deployment
+"C:\Program Files (x86)\Git\bin\git.exe" fetch --progress "origin" +refs/heads/r03_CARE_Screens/Deployment:refs/remotes/origin/r03_CARE_Screens/Deployment
 git reset --hard HEAD
 git clean -f -d
-"C:\Program Files (x86)\Git\bin\git.exe" pull "origin" +refs/heads/r02_CARE_Screens/Deployment:refs/remotes/origin/r02_CARE_Screens/Deployment
-"C:\Program Files (x86)\Git\bin\git.exe" checkout --force -B "r02_CARE_Screens/Deployment" "origin/r02_CARE_Screens/Deployment"
+"C:\Program Files (x86)\Git\bin\git.exe" pull "origin" +refs/heads/r03_CARE_Screens/Deployment:refs/remotes/origin/r03_CARE_Screens/Deployment
+"C:\Program Files (x86)\Git\bin\git.exe" checkout --force -B "r03_CARE_Screens/Deployment" "origin/r03_CARE_Screens/Deployment"
