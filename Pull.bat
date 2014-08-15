05a4d58038f11ca5ba0ef42f26b967617923465f
MarkGStacey <mark.stacey@aphelion.ws>
2014-08-15 17:49:14 +0000

    Pull

--- Pull.bat
+++ Pull.bat
@@ -1,5 +1,5 @@
-"C:\Program Files (x86)\Git\bin\git.exe" fetch --progress "origin" +refs/heads/r03_CARE_Screens/Deployment:refs/remotes/origin/r03_CARE_Screens/Deployment
+"C:\Program Files (x86)\Git\bin\git.exe" fetch --all
 git reset --hard HEAD
 git clean -f -d
-"C:\Program Files (x86)\Git\bin\git.exe" pull "origin" +refs/heads/r03_CARE_Screens/Deployment:refs/remotes/origin/r03_CARE_Screens/Deployment
-"C:\Program Files (x86)\Git\bin\git.exe" checkout --force -B "r03_CARE_Screens/Deployment" "origin/r03_CARE_Screens/Deployment"
+"C:\Program Files (x86)\Git\bin\git.exe" pull "origin" +refs/heads/r03_CARE_Reports/Deployment:refs/remotes/origin/r03_CARE_Reports/Deployment
+"C:\Program Files (x86)\Git\bin\git.exe" checkout --force -B "r03_CARE_Reports/Deployment" "origin/r03_CARE_Reports/Deployment"
