aaffe6d5369c78ffa06fc51c402d021f13d3f9b1
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-08-15 12:01:30 +0000

    Fixing Project ShortName nvarchar issue

---
+++ SQL Solution/Aphelion.MeerkatStaging.DB/Aphelion.MeerkatStaging.DB/Staging/Tables/FacilityType.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [Staging].[FacilityType] (
+    [FacilityType_ID] INT            NULL,
+    [BusinessKey]     NVARCHAR (400) NOT NULL,
+    [Code]            VARCHAR (50)   NOT NULL,
+    [Name]            VARCHAR (255)  NOT NULL
+);
+
