c522811f580a1d75be2a39ab6401a8cc18975d23
MarkGStacey <mark.stacey@aphelion.ws>
2014-08-16 10:50:29 +0000

    CodeDeploy

--- CodeIntoDeploy.bat
+++ CodeIntoDeploy.bat
@@ -3,5 +3,5 @@
 "C:\Program Files (x86)\Git\bin\git.exe" pull --rebase --progress "origin" +refs/heads/r03_CARE_Reports/Deployment:refs/remotes/origin/r03_CARE_Reports/Development
 "C:\Program Files (x86)\Git\bin\git.exe" checkout --force "r03_CARE_Reports/Deployment"
 "C:\Program Files (x86)\Git\bin\git.exe" merge r03_CARE_Reports/Development
-"C:\Program Files (x86)\Git\bin\git.exe" push --progress "origin" refs/heads/r03_CARE_Reports/Deployment:refs/heads/r03_CARE_Reports/Deployment
+"C:\Program Files (x86)\Git\bin\git.exe" push --progress "origin" refs/heads/r03_CARE_Reports/Deployment:refs/heads/r03_CARE_Reports/Deploymentfs
 "C:\Program Files (x86)\Git\bin\git.exe" checkout --force "r03_CARE_Reports/Development"
