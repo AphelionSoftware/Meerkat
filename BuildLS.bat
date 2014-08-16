e618e45dcb5525b8983decc5df67d8492d30c8e6
MarkGStacey <mark.stacey@aphelion.ws>
2014-08-16 09:55:26 +0000

    Building upgraded capture and admin

--- BuildLS.bat
+++ BuildLS.bat
@@ -1,7 +1,7 @@
-"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\MeerkatCapture_MH\Meerkat_Capture\Meerkat_Capture.lsxproj" /T:Rebuild /P:Configuration=Release > Deploy\CaptureBuild.txt
-"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\MeerkatCapture_MH\Meerkat_Capture\Meerkat_Capture.lsxproj" /T:Publish /P:Configuration=Release > Deploy\CapturePublish.txt
-"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\MeerkatAdmin_MH\MeerkatAdmin_2\MeerkatAdmin_2_1.lsxproj" /T:Rebuild /P:Configuration=Release > Deploy\AdminBuild.txt
-"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\MeerkatAdmin_MH\MeerkatAdmin_2\MeerkatAdmin_2_1.lsxproj" /T:Publish /P:Configuration=Release > Deploy\AdminPublish.txt
+"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\MeerkatCapture_MH\Meerkat_Capture\Meerkat_Capture.lsxtproj" /T:Rebuild /P:Configuration=Release > Deploy\CaptureBuild.txt
+"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\MeerkatCapture_MH\Meerkat_Capture\Meerkat_Capture.lsxtproj" /T:Publish /P:Configuration=Release > Deploy\CapturePublish.txt
+"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\MeerkatAdmin_MH\MeerkatAdmin_2\MeerkatAdmin_2_1.lsxtproj" /T:Rebuild /P:Configuration=Release > Deploy\AdminBuild.txt
+"C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\MeerkatAdmin_MH\MeerkatAdmin_2\MeerkatAdmin_2_1.lsxtproj" /T:Publish /P:Configuration=Release > Deploy\AdminPublish.txt
 "C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\Aphelion.Meerkat.FormBuilder\Aphelion.Meerkat.FormBuilder.lsxtproj" /T:Rebuild /P:Configuration=Release > Deploy\FormBuilderBuild.txt
 "C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\Aphelion.Meerkat.FormBuilder\Aphelion.Meerkat.FormBuilder.lsxtproj" /T:Publish /P:Configuration=Release > Deploy\FormBuilderPublish.txt
 "C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe" "Development\Aphelion.Meerkat.FormCapture\Aphelion.Meerkat.FormCapture\Aphelion.Meerkat.FormCapture.lsxtproj" /T:Rebuild /P:Configuration=Release > Deploy\FormCaptureBuild.txt
