0ac128712e13a78c8e5d84b192e01860efb19216
MarkGStacey <andrew.mutua@alternateltd.com>
2014-08-16 09:08:28 +0000

    Build

--- BuildETL.bat
+++ BuildETL.bat
@@ -1,5 +1,5 @@
 "C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv" ETL\Aphelion.Meerkat.SSIS\Aphelion.Meerkat.SSIS.sln /rebuild 
-MKDIR Deplot\ETL
+MKDIR Deploy\ETL
 COPY "ETL\Aphelion.Meerkat.SSIS\Aphelion.Meerkat.ETL\bin\Development\Aphelion.ispac" "Deploy\ETL\Aphelion.Meerkat.ETL.ispac" /Y 
 COPY "ETL\Aphelion.Meerkat.SSIS\Aphelion.FileWatcher_QueueInsert\Aphelion.FileWatcher_QueueInsert\bin\Development\Aphelion.ispac" "Deploy\ETL\Aphelion.FileWatcher_QueueInsert.ispac" /Y 
 
--- Build_Deploy.bat
+++ Build_Deploy.bat
@@ -1,12 +1,6 @@
-ECHO "Building DBs"
 Call BuildDB.bat
-ECHO Building Lightswitch"
 CALL BuildLS.bat
-ECHO Building ETL
 CALL BuildETL.bat
-Echo Deploying Lightswitch
 Call DeployDB.bat
-Echo Deploying Lightswitch
 CALL DeployLS.bat
-Echo Deploying ETL
 CALL DeployETL.bat
