1520ea779ab35548c0fac6f2ab5cf10c14a7fb27
MarkGStacey <mark.stacey@aphelion.ws>
2014-08-17 09:01:46 +0000

    ps

--- Installation/SP/05_Meerkat_FixReportDataConnections.ps1
+++ Installation/SP/05_Meerkat_FixReportDataConnections.ps1
@@ -1,8 +1,21 @@
-﻿Param(
-  [string]$sitecoll,
-  [string]$filePath,
-  [string]$library,
-  [string]$datasource
-)
-Add-PSSnapin microsoft.sharepoint.powershell
+﻿#Param(
+#  [string]$sitecoll,
+#  [string]$filePath,
+#  [string]$library,
+#  [string]$datasource
+#)
 
+$password = "L0pt@8192"
+$SPSite = "http://mgs-m6700/"
+
+$pwd = $password | ConvertTo-SecureString -AsPlainText -Force
+$crd = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "administrator",$pwd
+Enter-PSSession -ComputerName mgs-m6700 -Authentication CredSSP -Credential $crd
+Add-PSSnapin microsoft.sharepoint.powershell 
+
+#------------------------------------------------------------------------------------------- 
+# Name:            Get-SSRSDataSourceInfo 
+# Description:     This script will display the data source info for all the reports in a doc lib 
+# Usage:        Run the function with the -DocLibUrl parameter 
+# By:             Ivan Josipovic, softlanding.ca 
+#------------------------------------------------------------------------------------------- 
