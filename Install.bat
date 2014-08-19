65e13d1e33bfdfda4b148ab56c9709d99baadcc0
MarkGStacey <mark.stacey@aphelion.ws>
2014-08-19 06:39:41 +0000

    Batch

--- Install.bat
+++ Install.bat
@@ -1,7 +1,7 @@
 CALL powershell "& 'Installation\Install_SSIS.ps1'"
 CALL powershell "& 'Installation\SetupProxies.ps1'"
-C:\Program Files\7-Zip\7z.exe" x Installation\MegaMenu.7z -o"C:\inetpub\Meerkat_Megamenu" -y
-C:\Program Files\7-Zip\7z.exe" x Installation\NewsAgg.7z -o"C:\inetpub\Meerkat_NewsAgg" -y
+"C:\Program Files\7-Zip\7z.exe" x Installation\MegaMenu.7z -o"C:\inetpub\Meerkat_Megamenu" -y
+"C:\Program Files\7-Zip\7z.exe" x Installation\NewsAgg.7z -o"C:\inetpub\Meerkat_NewsAgg" -y
 CALL powershell "& 'Installation\Install_LSWebApps.ps1'"
 CALL powershell "& 'Installation\Install_LSWebApps.ps1'"
 CD Installation\SP
