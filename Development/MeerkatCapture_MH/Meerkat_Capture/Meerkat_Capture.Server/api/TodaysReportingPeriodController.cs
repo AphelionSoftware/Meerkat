5bfef244f2ca9794aa7e12c66e344ff9ab5689fc
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-08-31 12:03:01 +0000

    Date fixes

--- Development/MeerkatCapture_MH/Meerkat_Capture/Meerkat_Capture.Server/api/TodaysReportingPeriodController.cs
+++ Development/MeerkatCapture_MH/Meerkat_Capture/Meerkat_Capture.Server/api/TodaysReportingPeriodController.cs
@@ -4,7 +4,7 @@ namespace LightSwitchApplication.api
     using System;
     using System.Collections.Generic;
     using System.Web.Http;
-    using System.Linq;
+    using System.Linq; 
 
     public class TodaysReportingPeriodController : ApiController
     {
@@ -14,9 +14,22 @@ namespace LightSwitchApplication.api
             var now = DateTime.Now;
             using (var context = ServerApplicationContext.CreateContext())
             {
-                result = (from _ in context.DataWorkspace.MeerkatData.ReportingPeriods.GetQuery().Execute()
-                          where (now >= _.FirstCycleDate) && (now <= _.LastCycleDate)
-                          select _).Single();
+                try
+                {
+                    int nowID = (now.Year * 10000) + (now.Month * 100) + now.Day;
+
+                    /*result = (from _ in context.DataWorkspace.MeerkatData.ReportingPeriods.GetQuery().Execute()
+                              where (now >= _.FirstCycleDate) && (now <= _.LastCycleDate)
+                              select _).Single();*/
+                    result = (from _ in context.DataWorkspace.MeerkatData.ReportingPeriods.GetQuery().Execute()
+                              where (nowID >= _.StartDateID) && (nowID <= _.EndDateID)
+                              select _).Single();
+                }
+                catch (System.Exception ex)
+                {
+
+                    return 0;
+                }
             }
 
             return result.ID;
