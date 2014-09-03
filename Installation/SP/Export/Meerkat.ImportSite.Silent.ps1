9d1f149dd8decb380e07b3ea7fd8880a7f8e60ba
MarkGStacey <MarkGStacey@aphelion.bi>
2014-09-03 14:53:47 +0000

    Stles

--- Installation/SP/Export/Meerkat.ImportSite.Silent.ps1
+++ Installation/SP/Export/Meerkat.ImportSite.Silent.ps1
@@ -2,7 +2,8 @@
   [string]$name,
   [string]$sitecoll,
   [string]$filePath,
-  [string]$template
+  [string]$template,
+  [string]$masterpage
 )
 Add-PSSnapin microsoft.sharepoint.powershell
 
@@ -13,6 +14,12 @@ New-SPWeb –url "$sitecoll" -name "$name" -template $template
 #Write-Host $template
 #Import the site 
 Import-SPWeb -Identity "$sitecoll" -Path "$filePath"
+
+
+$web.CustomMasterUrl =  "/_catalogs/masterpage/$masterpage"  
+$web.MasterUrl = "/_catalogs/masterpage/$masterpage" 
+$web.Update() 
+
 
 #New-SPWeb –url "http://mgs-m6700/sites/Meerkat/Outcome3/test" -name "Test" -template STS#0
 #Import-SPWeb -Identity "http://mgs-m6700/sites/Meerkat/Outcome2/Test" -Path "D:\Dropbox\GitHub\Meerkat\Installation\SP\Export\meerkat_project.cmp"
