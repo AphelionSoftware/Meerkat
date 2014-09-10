bf669bfba998033e6b4832527f671008d5847934
MarkGStacey <MarkGStacey@aphelion.bi>
2014-09-10 18:05:20 +0000

    Tweaking menus

--- SQL Solution/Aphelion.Meerkat.DB/Meerkat/mm/Views/ALL_ProgrammeMenuCategory.sql
+++ SQL Solution/Aphelion.Meerkat.DB/Meerkat/mm/Views/ALL_ProgrammeMenuCategory.sql
@@ -15,7 +15,7 @@ AS
          do.Programme_ID, 
          OC.[ProgrammeSiteName] SiteName
   FROM   [app].[Sector] do 
-         LEFT JOIN [app].[Programme] AS OC 
+         JOIN [app].[Programme] AS OC 
                 ON do.Programme_ID = oc.[Programme_ID] 
 				WHERE do.Active = 1 AND oc.Active = 1
 				/*Hide sectors with no indicators*/
@@ -34,12 +34,24 @@ AS
   FROM   [app].[Programme] 
   WHERE Active = 1
 
+  UNION ALL 
+  /*Care specific. Sectors with no programme to CO level*/
+  SELECT Title = do.Code , 
+         Link = '', 
+         1 - do.Sector_ID + 5000 AS ID, 
+         do.Programme_ID, 
+         OC.[ProgrammeSiteName] SiteName
+  FROM   [app].[Sector] do 
+         JOIN [app].[Programme] AS OC 
+                ON do.Programme_ID is null
+				AND OC.BusinessKey = 'CO'
+				WHERE do.Active = 1 AND oc.Active = 1
+				/*Hide sectors with no indicators*/
+				AND EXISTS (SELECT 1 FROM app.Indicator I
+								LEFT JOIN app.SubSector SS
+								ON I.SubSector_ID = SS.SubSector_ID
+								WHERE I.Sector_ID = do.Sector_ID
+								OR SS.Sector_ID = do.Sector_ID)
 
 GO
-
-
-
-
-GO
-
 
