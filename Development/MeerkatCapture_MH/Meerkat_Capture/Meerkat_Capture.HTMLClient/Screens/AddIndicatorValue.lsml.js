a8499deaa8d785c490117a2a01ab052d608c2b9c
MarkGStacey <MarkGStacey@aphelion.bi>
2014-09-10 13:45:10 +0000

    Rolling security in

---
+++ Development/MeerkatCapture_MH/Meerkat_Capture/Meerkat_Capture.HTMLClient/Screens/AddIndicatorValue.lsml.js
@@ -0,0 +1,35 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+/// <reference path="LightSwitchTools.js" />
+
+myapp.AddIndicatorValue.created = function (screen) {
+
+    if (screen.IndicatorValue === undefined) {
+        screen.IndicatorValue = new myapp.IndicatorValue();
+    }
+
+    msls.application.lightswitchTools.setCommonAddScreenValues(screen);
+
+    $.getJSON("/api/TodaysReportingPeriod", function (data) {
+        myapp.activeDataWorkspace.MeerkatData.ReportingPeriods_SingleOrDefault(data).execute().then(function (reportingPeriod) {
+            screen.MaxReportingRangeID = reportingPeriod.results[0].EndDateID;
+            screen.ReportingPeriods.load().then(function () {
+                screen.IndicatorValue.setReportingPeriod(reportingPeriod.results[0]);
+            });
+        });
+    });
+
+    myapp.activeDataWorkspace.MeerkatData.Indicators_SingleOrDefault(screen.IndicatorId).execute().then(function (result) {
+        var indicator = result.results[0];
+        screen.IndicatorValue.setIndicator(indicator);
+    });
+};
+
+myapp.AddIndicatorValue.SearchLocationsTap_execute = function (screen) {
+    screen.IndicatorValue.Location = screen.Locations.selectedItem;
+    screen.closePopup();
+};
+
+myapp.AddIndicatorValue.SearchOrganizationsTap_execute = function (screen) {
+    screen.IndicatorValue.Organization = screen.Organizations.selectedItem;
+    screen.closePopup();
+};
---
+++ Development/MeerkatCapture_MH/Meerkat_Capture/Meerkat_Capture.HTMLClient/Screens/AddMilestoneValue.lsml.js
@@ -0,0 +1,35 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+/// <reference path="../Scripts/LightSwitchTools.js" />
+
+
+myapp.AddMilestoneValue.created = function (screen) {
+    if (screen.MilestoneValue === undefined) {
+        screen.MilestoneValue = new myapp.MilestoneValue();
+    }
+
+    msls.application.lightswitchTools.setCommonAddScreenValues(screen);
+    
+    $.getJSON("/api/TodaysReportingPeriod", function (data) {
+        myapp.activeDataWorkspace.MeerkatData.ReportingPeriods_SingleOrDefault(data).execute().then(function (reportingPeriod) {
+            screen.MaxReportingRange = reportingPeriod.results[0].EndDateID;
+            screen.ReportingPeriods.load().then(function () {
+                screen.MilestoneValue.setReportingPeriod(reportingPeriod.results[0]);
+            });
+        });
+    });
+
+    myapp.activeDataWorkspace.MeerkatData.Milestones_SingleOrDefault(screen.MilestoneId).execute().then(function (result) {
+        var mileStone = result.results[0];
+        screen.MilestoneValue.setMilestone(mileStone);
+    });
+};
+
+myapp.AddMilestoneValue.SearchOrganizationsTap_execute = function (screen) {
+    screen.MilestoneValue.Organization = screen.Organizations.selectedItem;
+    screen.closePopup();
+};
+
+myapp.AddMilestoneValue.SelectedLocationsTap_execute = function (screen) {
+    screen.MilestoneValue.Location = screen.Locations.selectedItem;
+    screen.closePopup();
+};
