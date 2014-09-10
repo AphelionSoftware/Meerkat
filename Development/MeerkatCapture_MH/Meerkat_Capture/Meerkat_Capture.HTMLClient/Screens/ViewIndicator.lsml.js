cb073c3f4fad689270da869725aa062094a06391
MarkGStacey <MarkGStacey@aphelion.bi>
2014-09-10 13:45:10 +0000

    Rolling security in

---
+++ Development/MeerkatCapture_MH/Meerkat_Capture/Meerkat_Capture.HTMLClient/Screens/ViewIndicator.lsml.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="../GeneratedArtifacts/viewModel.js" />
+
+function ShowEditIndicatorScreen(iv, screen) {
+    myapp.showAddIndicatorValue(iv, screen.Indicator.IndicatorID, {
+        afterClose: function () {
+            screen.details.properties.IndicatorValues.load();
+        }
+    });
+}
+
+myapp.ViewIndicator.AddIndicatorValue_execute = function (screen) {
+    ShowEditIndicatorScreen(undefined, screen);
+};
+
+myapp.ViewIndicator.ShowHome_postRender = function (element, contentItem) {
+    msls.application.lightswitchTools.homeIcon(element); 
+};
