7942057b72138492b710d4d966b435c457c1b569
MarkGStacey <Mark.Stacey@Aphelion.WS>
2014-10-19 13:42:02 +0000

    CB Changes

---
+++ SQL Solution/Aphelion.Meerkat.DB/Meerkat/forms/Views/vwUnfinishedForms.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW forms.vwUnfinishedForms
+AS
+
+SELECT FR.FormResponse_ID, FR.Form_ID, FR.ProjectID  FROM forms.FormResponse FR
+WHERE (
+
+select count(*) From forms.Response R
+WHERE r.FormResponse_ID = FR.FormResponse_ID)
+<
+
+(select count(*) from forms.Question Q
+INNER join forms.Category C
+on Q.Category_ID = C.Category_ID
+WHERE C.Form_ID = FR.Form_ID)
