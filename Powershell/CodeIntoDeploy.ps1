34a9538a4fef7c55fbc031c1a4f392c7eb8748b8
christinaleo <christina.leo@aphelion.bi>
2014-12-22 16:37:41 +0000

    test parameters removed

--- Powershell/CodeIntoDeploy.ps1
+++ Powershell/CodeIntoDeploy.ps1
@@ -1,13 +1,13 @@
-﻿
+﻿<#
 $GitFolder = "E:\Github\Meerkat"
 $BranchPrefix = "r03_CARE_Reports"
+#>
 
-<#
 Param (
     [Parameter(Mandatory=$True)][String]$GitFolder,
     [Parameter(Mandatory=$True)][String]$BranchPrefix
       )
-#>
+
 cd $GitFolder
 
 
@@ -31,4 +31,5 @@ git push --progress "origin" $DepToDep
 git checkout --force $BranchPrefix"/Development"
 git push --progress "origin"$DepToDev 
 git push --recurse-submodules=check --progress "origin" $DepToDep
+git push --recurse-submodules=check --progress "origin" $DevToDev
 
