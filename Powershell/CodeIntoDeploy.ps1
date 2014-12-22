dd7d9fe26dae6665256bee5150691865e3111421
christinaleo <christina.leo@aphelion.bi>
2014-12-22 15:51:34 +0000

    added parameter to specify git repo folder

--- Powershell/CodeIntoDeploy.ps1
+++ Powershell/CodeIntoDeploy.ps1
@@ -1,9 +1,14 @@
-﻿git fetch --all
-git pull --rebase --progress "origin" +refs/heads/r02_Joint/Deployment:+refs/heads/r02_Joint/Deployment:refs/remotes/origin/r02_Joint/Deployment
-git checkout --force "r02_Joint/Deployment"
-git pull --rebase --progress "origin" +refs/heads/r02_Joint/Deployment:refs/remotes/origin/r01_Finance/Development
-git merge r01_Finance/Development
-git push --progress "origin" refs/heads/r02_Joint/Deployment:refs/heads/r02_Joint/Deployment
-git checkout --force "r01_Finance/Development"
-git push --progress "origin" refs/heads/r01_Finance/Development:refs/remotes/origin/r01_Finance/Development
-git push --recurse-submodules=check --progress "origin" refs/heads/R01_Finance/Development:refs/heads/R01_Finance/Development
+﻿Param (
+    [Parameter(Mandatory=$True)][String]$GitFolder
+      )
+cd $GitFolder
+
+git fetch --all
+git pull --rebase --progress "origin" +refs/heads/r03_CARE_Reports/Deployment:+refs/heads/r03_CARE_Reports/Deployment:refs/remotes/origin/r03_CARE_Reports/Deployment
+git checkout --force "r03_CARE_Reports/Deployment"
+git pull --rebase --progress "origin" +refs/heads/r03_CARE_Reports/Deployment:refs/remotes/origin/r03_CARE_Reports/Development
+git merge r03_CARE_Reports/Development
+git push --progress "origin" refs/heads/r03_CARE_Reports/Deployment:refs/heads/r03_CARE_Reports/Deployment
+git checkout --force "r03_CARE_Reports/Development"
+git push --progress "origin" refs/heads/r03_CARE_Reports/Development:refs/remotes/origin/r03_CARE_Reports/Development
+git push --recurse-submodules=check --progress "origin" refs/heads/r03_CARE_Reports/Development:refs/heads/r03_CARE_Reports/Development
--- Powershell/PullDeploymentBranch.ps1
+++ Powershell/PullDeploymentBranch.ps1
@@ -1,5 +1,10 @@
-﻿git fetch --all
+﻿Param (
+    [Parameter(Mandatory=$True)][String]$GitFolder
+      )
+cd $GitFolder
+
+git fetch --all
 git reset --hard HEAD
 git clean -f -d
-git pull "origin" +refs/heads/r02_Joint/Deployment:refs/remotes/origin/r02_Joint/Deployment
-git checkout --force -B "r02_Joint/Deployment" "origin/r02_Joint/Deployment"
+git pull "origin" +refs/heads/r03_CARE_Reports/Deployment:refs/remotes/origin/r02_Joint/Deployment
+git checkout --force -B "r03_CARE_Reports/Deployment" "origin/r03_CARE_Reports/Deployment"
