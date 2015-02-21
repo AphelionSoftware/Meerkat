01f79fd56932c7f9e69b0352da27c14b511f7782
MarkGStacey <Mark.Stacey@Aphelion.WS>
2015-02-21 18:34:09 +0000

    WOrking

---
+++ SQL Solution/Aphelion.Meerkat.DB/Meerkat/PostDeployScripts/deploy.DataCleanup.sql
@@ -0,0 +1,45 @@
+﻿ 
+UPDATE [RBM].[PeopleReachedValues]
+SET Gender_ID = G.Gender_ID
+FROM  disagg.Gender g
+WHERE PeopleReachedValues.Gender_ID NOT IN (0,1)
+AND G.Code = 'N/A'
+ 
+UPDATE [RBM].[MilestoneValues]
+SET Gender_ID = G.Gender_ID
+FROM  disagg.Gender g
+WHERE MilestoneValues.Gender_ID NOT IN (0,1)
+AND G.Code = 'N/A'
+ 
+UPDATE [RBM].[IndicatorValues]
+SET Gender_ID = G.Gender_ID
+FROM  disagg.Gender g
+WHERE IndicatorValues.Gender_ID NOT IN (0,1)
+AND G.Code = 'N/A'
+ 
+UPDATE [forms].[Form]
+SET Gender_ID = G.Gender_ID
+FROM  disagg.Gender g
+WHERE Form.Gender_ID NOT IN (0,1)
+AND G.Code = 'N/A'
+ 
+UPDATE [forms].[FormResponse]
+SET Gender_ID = G.Gender_ID
+FROM  disagg.Gender g
+WHERE FormResponse.Gender_ID NOT IN (0,1)
+AND G.Code = 'N/A'
+ 
+UPDATE [forms].[Question]
+SET Gender_ID = G.Gender_ID
+FROM  disagg.Gender g
+WHERE Question.Gender_ID NOT IN (0,1)
+AND G.Code = 'N/A'
+ 
+UPDATE [forms].[EventRegister]
+SET Gender_ID = G.Gender_ID
+FROM  disagg.Gender g
+WHERE EventRegister.Gender_ID NOT IN (0,1)
+AND G.Code = 'N/A'
+
+DELETE FROM disagg.Gender
+WHERE CODE  NOT IN ('M', 'F', 'N/A')
