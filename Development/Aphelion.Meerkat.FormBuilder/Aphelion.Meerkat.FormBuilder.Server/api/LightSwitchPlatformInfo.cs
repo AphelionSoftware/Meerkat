8f27332e723620e5def95b0b813c2af7de37c7c1
MarkGStacey <Mark.Stacey@Aphelion.WS>
2015-03-01 15:28:56 +0000

    Tested form builder and capture per Oliver

---
+++ Development/Aphelion.Meerkat.FormBuilder/Aphelion.Meerkat.FormBuilder.Server/api/LightSwitchPlatformInfo.cs
@@ -0,0 +1,11 @@
+﻿
+namespace LightSwitchApplication.api
+{
+    public class LightSwitchPlatformInfo
+    {
+        public string Version { get; set; }
+        public string Deployed { get; set; }
+        public string UserName { get; set; }
+        public string ClientCode { get; set; }
+    }
+}
