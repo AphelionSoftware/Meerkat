e314e876f6e9b9a4c032925e3cecd56976ca51ba
MarkGStacey <Mark.Stacey@Aphelion.WS>
2015-05-25 15:46:59 +0000

    Commit for deploy

--- CommitPushDeployment.bat
+++ CommitPushDeployment.bat
@@ -1,8 +1,7 @@
 "C:\Program Files (x86)\Git\bin\git.exe" add --all
 "C:\Program Files (x86)\Git\bin\git.exe" commit -am "Commit for deploy"
 "C:\Program Files (x86)\Git\bin\git.exe" checkout --force "r03_CARE_Reports/Deployment"
-"C:\Program Files (x86)\Git\bin\git.exe" pull --merge --progress "origin" +refs/heads/r03_CARE_Reports/Deployment:refs/remotes/origin/r03_CARE_Reports/Development
+"C:\Program Files (x86)\Git\bin\git.exe" pull --progress "origin" +refs/heads/r03_CARE_Reports/Deployment:refs/remotes/origin/r03_CARE_Reports/Development
 "C:\Program Files (x86)\Git\bin\git.exe" merge r03_CARE_Reports/Development
 "C:\Program Files (x86)\Git\bin\git.exe" push --progress "origin" refs/heads/r03_CARE_Reports/Deployment:refs/heads/r03_CARE_Reports/Deployment
 "C:\Program Files (x86)\Git\bin\git.exe" checkout --force "r03_CARE_Reports/Development"
-"C:\Program Files (x86)\Git\bin\git.exe" push --progress "origin" refs/heads/r03_CARE_Reports/Development:refs/heads/r03_CARE_Reports/Development
